649e38d7d61f1b21d2f7c40f72047cccd022854a
agusauzm <61537211+agusauzm@users.noreply.github.com>
2020-03-27 00:13:19 +0100

    Réduction de la taille du main

--- PAD:main.cpp
+++ PAD:main.cpp
@@ -52,10 +52,9 @@ void start_KB_LCD()
 void get_kbd_str(char *promp, char *bufr)
 {
       start_KB_LCD();
-      Keyboard_init(10, 50);
+      Keyboard_init(10, 0);
       while(1)  {
           key = Keyboard_handler(promp, bufr);
-          if (key == KEY_ENTER_VALUE)   break;    // to go to another screen ?
           HAL_Delay(10);  
       }
  }  
@@ -64,50 +63,7 @@ void get_kbd_str(char *promp, char *bufr)
 /* Programme de base */
 int main(void)
 {
-#define STATSIZE 126
-                   
-      char  sbuf[126];  
-      char  dbgtlk[126];
-      
-      char  tstr[126];
-      char  bstr[32];
-      char  cstr[32];
-      char  estr[32];
-      char  pbstr[32];
-      int   ii;
-      
-         char *rawp;
-    char *csvp;
-    int   rawq; 
-    int   bi, ci, ei; 
-    float pbf, bf, cf, ef;
-    float pbar[STATSIZE];
-    float bar[STATSIZE];
-    float car[STATSIZE];
-    float ear[STATSIZE];
-    int duds = 0;
-    char  gotc;
-    
-    float pb_mn, pb_sd, b_mn, b_sd, c_mn, c_sd, e_mn, e_sd;
-    
-      char msg_b[126];
-      int  u_tries;
-      char u_name[126];
-      char u_passwd[126];
-      char out_str[126];
-      char out_str2[126];
-      
-       
       start_KB_LCD(); // Nouvel ecran ( noir )
       BSP_LCD_SetFont(&Font24);
       get_kbd_str("Controleur MIDI", kb_buffer); // affiche le clavier + etiquette en haut de l'écran
-      strcpy(u_name, kb_buffer); //char * strcpy ( char * destination, const char * source ) copies the C string pointed by source into the array pointed by destination
-      kb_buffer[0] = '\0';
-     
-      start_KB_LCD();
-      BSP_LCD_SetFont(&Font24);
-      get_kbd_str("USE JOYSTICK TO ENTER PASSWORD:", kb_buffer);
-      strcpy(u_passwd, kb_buffer);
-      kb_buffer[0] = '\0';
-      
 }
