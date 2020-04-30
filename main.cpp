156eac68cb06a3b55d8e4aedaacf083ff3bbda61
agusauzm <61537211+agusauzm@users.noreply.github.com>
2020-04-30 08:17:37 +0200

    MAIN.CPP FINAL

---
+++ main.cpp
@@ -0,0 +1,69 @@
+/*
+ *******************************************************************************
+ * Un exemple du clavier à l’écran sur le panneau de démonstration STM32F746NG Discovery
+ Utilisation d’un ensemble standard de bibliothèques BSP et d’une
+ Bibliothèques "keyboard.h"
+ *******************************************************************************
+ */
+
+/* Ajouter une bibliothèque pour travailler avec LCD */
+#include "stm32746g_discovery_lcd.h"
+/* Ajout d’une bibliothèque de claviers à l’écra */
+#include "keyboard.h"
+
+#include "mbed.h"
+
+   // no such file: #include "USBSerial.h"
+
+/* Modifications auxiliaires */
+uint8_t i;
+uint8_t key;
+uint8_t  len;
+
+/* Mémoire tampon personnalisée */
+char kb_buffer[45];
+
+void start_KB_LCD()  
+{
+    /* Initialisation de l’affichage
+       Nous éteignons l’écran.
+       Nous avons mis en place les première et deuxième couches de l’écran.*/
+    BSP_LCD_Init();
+    BSP_LCD_LayerDefaultInit(0, LCD_FB_START_ADDRESS);
+    BSP_LCD_LayerDefaultInit(1, LCD_FB_START_ADDRESS + (BSP_LCD_GetXSize() * BSP_LCD_GetYSize() * 4));
+    BSP_LCD_DisplayOn();
+    BSP_LCD_SelectLayer(0);
+    BSP_LCD_Clear(LCD_COLOR_WHITE);
+    BSP_LCD_SelectLayer(1);
+    BSP_LCD_Clear(LCD_COLOR_BLACK);
+    BSP_LCD_SetTransparency(0, 0);
+    BSP_LCD_SetTransparency(1, 100);
+    /* Définir la couleur de fond et la couleur du texte de l’écran, ainsi que la police  */
+    BSP_LCD_SetTextColor(LCD_COLOR_BLUE);
+    BSP_LCD_SetBackColor(LCD_COLOR_WHITE);
+    BSP_LCD_SetFont(&Font16);
+    
+    /* Initialisation de l’écran tactile*/
+    BSP_TS_Init(BSP_LCD_GetXSize(), BSP_LCD_GetYSize());
+   
+}
+
+
+void get_kbd_str(char *promp, char *bufr)
+{
+      start_KB_LCD();
+      Keyboard_init(10, 0);
+      while(1)  {
+          key = Keyboard_handler(promp, bufr);
+          HAL_Delay(10);  
+      }
+ }  
+
+
+/* Programme de base */
+int main(void)
+{
+      start_KB_LCD(); // Nouvel ecran ( noir )
+      BSP_LCD_SetFont(&Font24);
+      get_kbd_str("Controleur MIDI", kb_buffer); // affiche le clavier + etiquette en haut de l'écran
+}
